24e7a71b07f4349b77eb2837846138eb56d6428f
v.buchnev <valentinbuchnev0904@gmail.com>
2024-05-31 14:58:43 +0800

    axios default url

--- src/stores/services.js
+++ src/stores/services.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import {defineStore} from "pinia";
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
-
 export const useServicesStore = defineStore("services", {
   state: () => ({
     services: {},
