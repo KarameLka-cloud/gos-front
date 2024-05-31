04f0a11cc1f5a5581e3a93884cab1a3b2665d9bd
v.buchnev <valentinbuchnev0904@gmail.com>
2024-05-31 14:58:24 +0800

    axios default url

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import "./styles/reset.css";
 import "./styles/normalize.css";
 import "./styles/style.css";
 import App from "./App.vue";
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 const pinia = createPinia();
 const app = createApp(App);
